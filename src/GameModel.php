c0e77b29d3e8e0124c661bd49d5fb4e2b838f820
Andrew Fe <f883@yandex.com>
2019-08-05 17:30:06 +0000

    update

--- src/GameModel.php
+++ src/GameModel.php
@@ -1,70 +1,72 @@
 <?php
 require 'vendor/autoload.php';
 
-class GameModel{
+class GameModel
+{
     private static $dbAddress = 'mongodb://192.168.101.160:32768';
 
-    public static function addGame($data){
+    public static function addGame($data)
+    {
         $client = new MongoDB\Client(GameModel::$dbAddress);
         $collection = $client->test_db->games;
-        
-        $filter=[];
+
+        $filter = [];
         $options = ['sort' => ['id' => -1]]; // -1 is for DESC
         $id = $collection->findOne($filter, $options)->id;
         $id++;
-        
-        $result = $collection->insertOne( 
-            [ 'id' => $id, 'name' => $data->name ] 
+
+        $result = $collection->insertOne(
+            ['id' => $id, 'name' => $data->name]
         );
         return $id;
     }
 
-    public static function changeGameData($data){
+    public static function changeGameData($data)
+    {
         $client = new MongoDB\Client(GameModel::$dbAddress);
         $collection = $client->test_db->games;
-        $id = (int)$data->id;
+        $id = (int) $data->id;
         $game = $collection->findOne(array('id' => $id));
-        if (!is_null($game)){
+        if (!is_null($game)) {
             $game = $collection->updateOne(
-                [ 'id' => $id ],
-                [ '$set' => [ 
-                    'name' => $data->name
-                    ] 
+                ['id' => $id],
+                ['$set' => [
+                    'name' => $data->name,
+                ],
                 ]
             );
             $game = $collection->findOne(array('id' => $id));
             unset($game['_id']); // удаляем метку mongodb
             return $game;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public static function getGame($id){
+    public static function getGame($id)
+    {
         $client = new MongoDB\Client(GameModel::$dbAddress);
         $collection = $client->test_db->games;
-        $game = $collection->findOne(['id' => (int)$id]);
-        if (!is_null($game)){
+        $game = $collection->findOne(['id' => (int) $id]);
+        if (!is_null($game)) {
             unset($game['_id']); // удаляем метку mongodb
             return $game;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public static function deleteGame($id){
+    public static function deleteGame($id)
+    {
         $client = new MongoDB\Client(GameModel::$dbAddress);
-        $collection = $client->test_db->games;        
-        $game = $collection->findOne(['id' => (int)$id]);
+        $collection = $client->test_db->games;
+        $game = $collection->findOne(['id' => (int) $id]);
 
-        if (!is_null($game)){
-            $collection->deleteOne(['id' => (int)$id]);
+        if (!is_null($game)) {
+            $collection->deleteOne(['id' => (int) $id]);
             unset($game['_id']); // удаляем метку mongodb
             return $game;
-        }
-        else{
+        } else {
             return false;
         }
     }
--- src/GetGameController.php
+++ src/GetGameController.php
@@ -1,23 +1,24 @@
-<?php 
+<?php
 
 require_once "IController.php";
 require_once "GameModel.php";
 
-class GetGameController implements IController{
-    public static function execute($path, $params, $data = []){
+class GetGameController implements IController
+{
+    public static function execute($path, $params, $data = [])
+    {
         $id = explode('/', $path)[2];
         $res = GameModel::getGame($id);
 
         header('Content-Type: application/json');
-        if ($res){
-            http_response_code(200);
+
+        if ($res) {http_response_code(200);
             echo json_encode($res);
-        }
-        else{
+        } else {
             http_response_code(404);
             echo json_encode([
-                "ERROR" => 'Game not found.'
+                "ERROR" => 'Game not found.',
             ]);
         }
-    } 
+    }
 }
